ca15b2620fafd7ef0b0e1fee086b898d6f6ae1d1
Eduardo Scovino <eduardo.scovino@gmail.com>
2021-04-15 20:43:37 -0300

    buttons + answer page + css

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,14 @@ class QuestionsController < ApplicationController
 
   def ask
   end
+  
+  def answer
+    if params[:answer].downcase == 'i am going to work right now'
+      @answer = 'Great!'
+    elsif params[:answer].downcase.include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
